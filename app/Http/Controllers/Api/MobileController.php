36ba4718b3b484cbc2697b66d571a65e4af24161
chatomzcoding <coding.chatomz@gmail.com>
2021-12-17 10:25:31 +0700

    join user dan client di cek login

--- app/Http/Controllers/Api/MobileController.php
+++ app/Http/Controllers/Api/MobileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
 use App\Models\Session;
 use App\Models\User;
 use App\Models\Userakses;
@@ -19,17 +20,25 @@ class MobileController extends Controller
         if ($user) {
             $password   = $user->password;
             if (password_verify($request->password,$password)) {
-                Session::create([
-                    'id' => $user->id.time(),
-                    'user_id' => $user->id,
-                    'payload' => 1,
-                    'user_agent' => $request->nama_device,
-                    'last_activity' => 0,
-                ]);
-                return response()->json([
+                // Session::create([
+                //     'id' => $user->id.time(),
+                //     'user_id' => $user->id,
+                //     'payload' => 1,
+                //     'user_agent' => $request->nama_device,
+                //     'last_activity' => 0,
+                // ]);
+                $success    = [
                     'success' => 1,
                     'message' => 'success'
-                ]);
+                ];
+                $userakses  = Userakses::where('user_id',$user->id)->first();
+                $client     = Client::find($userakses->client_id);
+                $gabungan   = [
+                    'user' => $user,
+                    'client' => $client,
+                ];
+                $result     = array_merge($success,$gabungan);
+                return response()->json($result);
             } else {
                 return response()->json([
                     'success' => 0,
